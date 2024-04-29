fab76f7cf722bd9dc4ecb8bae3a354d4f432d7a5
devthigas502 <thiagodev502@gmail.com>
2024-04-29 20:22:01 -0300

    chore: mudando o prefixo da rota de wp-json para json

--- src/functions.php
+++ src/functions.php
@@ -1,8 +1,17 @@
 <?php 
 
 
+# removendo rotas iniciais
 remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 
+
+# mudando o prefixo da rota
+function change_name_route(){
+    return "json";
+}
+
+# change_name_route função de callback
+add_filter('rest_url_prefix', 'change_name_route');
 
 ?>
