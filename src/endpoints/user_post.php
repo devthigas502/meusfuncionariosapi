2f2fbf29cf471b497c08cbaa0b2c57abed035c78
devthigas502 <thiagodev502@gmail.com>
2024-04-29 21:49:05 -0300

    feature: registrando a api user no endpoint

--- src/endpoints/user_post.php
+++ src/endpoints/user_post.php
@@ -1,1 +1,25 @@
-<?= "user rodando..." ?>
+<?php
+
+
+function teste($data){
+    $nome = $data['nome'];
+
+    if(empty($nome)){
+        $response = new WP_Error('error', 'está vazio', ['status' => 406]);
+        return rest_ensure_response($response);
+    }
+
+}
+
+
+function register_api_user_post(){
+    register_rest_route('api', 'user', [
+        'methods' => 'POST',
+        'callback' => 'teste'
+    ]);
+}
+
+add_action('rest_api_init', 'register_api_user_post');
+
+
+?>
