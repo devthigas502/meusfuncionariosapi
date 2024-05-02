ebf302287e3b74409ea69451cc9347a45ecdfc7b
devthigas502 <thiagodev502@gmail.com>
2024-05-02 09:44:25 -0300

    feature: finalizando rota user_get

--- src/endpoints/user_get.php
+++ src/endpoints/user_get.php
@@ -1,10 +1,32 @@
 <?php
 
 
-function register_user_get(){
-    echo "teste";
+function user_get(){
+    $user = wp_get_current_user();
+
+    if($user->ID == 0){
+        $response = new WP_Error('error','Usuário não autenticado',['status' => 401]);
+        return rest_ensure_response($response);
+    }
+
+    $response = [
+        'id' => $user->ID
+        'name' => $user->display_name
+        'email' => $user->user_email
+        'registered' => $user->user_registered
+    ];
+
+    return rest_ensure_response($response);
 }
 
+
+function register_user_get(){
+    register_rest_route('api', 'user', [
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'user_get'
+
+    ]);
+}
 
 add_action('rest_api_init', 'register_user_get');
 
